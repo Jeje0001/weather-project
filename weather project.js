b803b10dc297335d54e8cc4bad6349a959cca033
Jeje0001 <Jejeayomide16@outlook.com>
2024-04-30 23:36:46 -0400

    kk
    kk

--- weather project.js
+++ weather project.js
@@ -1,14 +1,28 @@
 // apikey="7ff6d15c053625fb8a83dc610c55b67a"
 // https://api.openweathermap.org/data/2.5/weather?q={cityname}&appid={API key}
 const apikey="7ff6d15c053625fb8a83dc610c55b67a"
-const apiurl="https://api.openweathermap.org/data/2.5/weather?units=metric&q=germany"
+const apiurl="https://api.openweathermap.org/data/2.5/weather?units=imperial&q="
 
+const searchbox=document.querySelector(".sea")
 
-async function checkweather(){
-    const response = await fetch(apiurl +`&appid=${apikey}`)
+const searchbtn=document.querySelector(".button")
+
+async function checkweather(city){
+    const response = await fetch(apiurl +city+`&appid=${apikey}`)
     let data=await response.json()
-    let d=document.querySelector(".city").innerHTML=d
+    document.querySelector(".city").innerHTML=data.name
+    document.querySelector(".temp").innerHTML=data.main.temp + "°F"
+    document.querySelector(".humidity").innerHTML=data.main.humidity +  "%"
+    document.querySelector(".wind").innerHTML=data.wind.speed + "km/h"
+
+
 
     console.log(data)
 }
-//continue at 24:26
+
+searchbtn.addEventListener("click",()=>{
+    checkweather(searchbox.value);
+
+})
+
+//continue at 30:26
